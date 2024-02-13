39d49c30508fdd6cea5cc92534be18b3e077b8d4
Jonathan Niklasson Godar <jonathan.godar@gmail.com>
2024-02-13 10:50:47 +0100

    Major refactoring on uppg2 to comply with project spec

--- uppg2.m
+++ uppg2.m
@@ -1,117 +1,53 @@
-aCoords = [-1; 1.5];
-bCoords = [3; 0.5];
-cCoords = [0; -2];
+a = [-1; 1.5];
+b = [3; 0.5];
+c = [0; -2];
 
-aRadius = 1;
-bRadius = 1.2;
-cRadius = 1.7;
+ra = 1;
+rb = 1.2;
+rc = 1.7;
 
-tic;
-a_b_guess = aCoords + [0;1];
-b_a_guess = bCoords + [0;1];
+[l, X] = langd(ra, rb, rc, a, b, c);
 
-a_b_coords= punkter([a_b_guess; b_a_guess], aRadius, bRadius, aCoords, bCoords);
+disp("Längd");
+disp(l);
 
-a_c_guess = aCoords + [-1;0];
-c_a_guess = cCoords + [-1;0];
-a_c_coords = punkter([a_c_guess; c_a_guess], aRadius, cRadius, aCoords, cCoords);
-
-b_c_guess = bCoords + [0;-1];
-c_b_guess = cCoords + [1;0];
-b_c_coords = punkter([b_c_guess; c_b_guess], bRadius, cRadius, bCoords, cCoords);
-toc
-
-a_to_b = a_b_coords(1:2);
-b_to_a = a_b_coords(3:4);
-
-a_to_c = a_c_coords(1:2);
-c_to_a = a_c_coords(3:4);
-
-b_to_c = b_c_coords(1:2);
-c_to_b = b_c_coords(3:4);
-
-drawSphere(aCoords, aRadius, 'b');
-hold on 
 axis equal
-drawSphere(bCoords, bRadius, 'b');
-drawSphere(cCoords, cRadius, 'b');
+hold on
+for i = [1:3]
+    line(X(1:2:end,i), X(2:2:end,i));
+end
 
-plot_line_from_matrix([a_to_b, b_to_a]);
-plot_line_from_matrix([a_to_c, c_to_a]);
-plot_line_from_matrix([c_to_b, b_to_c]);
-hold off
+drawSphere(a, ra);
+drawSphere(b, rb);
+drawSphere(c, rc);
 
-
-points_list = [a_to_b, a_to_c, b_to_a, b_to_c, c_to_a, c_to_b];
-circle_center_list = [aCoords, bCoords, cCoords];
-radius_list = [aRadius, bRadius, cRadius];
-
-disp("Svar: ")
-calculate_rope_length(circle_center_list, radius_list, points_list)
-
-% a_perim = calculate_perimiter(aCoords, aRadius, b_to_c, b_to_a);
-% b_perim = calculate_perimiter(bCoords, bRadius, b_to_c, b_to_a);
-
-% line([a_to_b(1) a_to_b(3)],[a_to_b(2) a_to_b(4)]);
-% line([a_to_c(1) a_to_c(3)],[a_to_c(2) a_to_c(4)]);
-% line([b_to_c(1) b_to_c(3)],[b_to_c(2) b_to_c(4)]);
-% hold off
-
-% calculate_perimiter(bCoords, bRadius, b_to_c, b_to_a)
-
-
-% b_to_a
-
-% a_to_c
-% c_to_a
-
-% b_to_c
-% c_to_b
-
-
-% drawSphere(bCoords, bRadius, 'b')
-% drawSphere(cCoords, cRadius,'g')
-
-
-
-% X = punkter([0.1,4,2.1,0.9]',1.5,0.8,[-1.5,3],[1,1])
-% hold on
-% fplot(@(t) 1.5*cos(t)-1.5, @(t) 1.5*sin(t)+3)
-% hold on
-% axis equal
-% fplot(@(t) 0.8*cos(t)+1, @(t) 0.8*sin(t)+1)
-% hold on
-% line([X(1) X(3)],[X(2) X(4)])
+%% Experimentell störningsanalys
 
 
 % Kod för uppgift 2...
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 function [L,X]=langd(ra,rb,rc,a,b,c)
+    a_b_guess = get_start_guesses(a, b, c);
+    a_b_coords= punkter(a_b_guess, ra, rb, a, b);
+    
+    a_c_guess = get_start_guesses(a, c, b);
+    a_c_coords = punkter(a_c_guess, ra, rc, a, c);
 
-%  Indata:
-%
-%  ra - radie för cirkel a (skalär)
-%  rb - radie för cirkel a (skalär)
-%  rc - radie för cirkel a (skalär)
-%  a  - kolumnvektor med koordinater för mittpunkt a (xa,ya)^T
-%  b  - kolumnvektor med koordinater för mittpunkt b (xb,yb)^T
-%  c  - kolumnvektor med koordinater för mittpunkt c (xc,yc)^T
-%
-%  Utdata:
-%
-%  L - längden på snöret (skalär)
-%  X - De tre lösningsvektorerna samlade i en 
-%      matris (4 rader, 3 kolumner), X=[Xab, Xbc, Xca]
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% kod...
+    b_c_guess = get_start_guesses(b, c, a);
+    b_c_coords = punkter(b_c_guess, rb, rc, b, c);
 
+    points_list = [a_b_coords(1:2), a_c_coords(1:2), a_b_coords(3:4), b_c_coords(1:2), a_c_coords(3:4), b_c_coords(3:4)];
+    L = calculate_rope_length([a, b, c], [ra, rb, rc], points_list);
+
+    X =  [a_b_coords, b_c_coords, a_c_coords];
+end
+
+
+function guesses = get_start_guesses(from, to, other)
+    from_to = from + (from - other);
+    to_from = to + (to - other);
+
+    guesses = [from_to; to_from];
 end
 
 
@@ -121,10 +57,6 @@ function sector = calculate_circle_sector(center, radius, point_a, point_b)
 
     cos_theta = dot(center_to_a, center_to_b) / (norm(center_to_a) * norm(center_to_b));
     theta= acos(cos_theta);
-    % norm(point_a - center)
-    % norm(point_b - center)
-    % dot(norm(point_a - center), norm(point_b - center))
-    % angle
     sector = theta * radius;
 end
 
@@ -134,9 +66,6 @@ function rope_length = calculate_rope_length(circle_center_list,circle_radii_lis
         circle_sector_length = calculate_circle_sector(circle_center_list(:, i), circle_radii_list(i),points_list(:,2*i-1),points_list(:,2*i));
         sum_of_circle_sector_length = sum_of_circle_sector_length + circle_sector_length;
     end
-    % circle_sector_length = calculate_circle_sector(circle_center_list(1), circle_radii_list(1),points_list(:,1),points_list(:,2))
-    % circle_sector_length = calculate_circle_sector(circle_center_list(:, 2), circle_radii_list(2),points_list(:,3),points_list(:,4))   
-    % circle_sector_length = calculate_circle_sector(circle_center_list(3), circle_radii_list(3),points_list(:,5),points_list(:,6))
     
     rope_segment_length = norm(points_list(:,1) - points_list(:,3));
     rope_segment_length = rope_segment_length + norm(points_list(:,4) - points_list(:,6));
@@ -153,12 +82,6 @@ function [Xrot, iter] = punkter(X0,ra,rb,a,b)
     b1 = b(1);
     b2 = b(2);
 
-		% x1 <- x1(1)
-		% y1 <- x1(2)
-
-		% x2 <- x2(1)
-		% y2 <- x2(2)
-
     F = @(x) [(x(1)-a1)^2+(x(2)-a2)^2-ra^2;
               (x(3)-b1)^2+(x(4)-b2)^2-rb^2;
               (x(1)-x(3))*(x(1)-a1)+(x(2)-x(4))*(x(2)-a2);
@@ -169,64 +92,28 @@ function [Xrot, iter] = punkter(X0,ra,rb,a,b)
                      2*x(1)-a1-x(3) 2*x(2)-a2-x(4) a1-x(1) a2-x(2);
                      x(3)-b1 x(4)-b2 x(1)+b1-2*x(3) x(2)+b2-2*x(4)];
 
-		x = X0;
+    x = X0;
 
-        xList = [x];
-		tolerance = 1e-10;
-        iterations = 0;
-		xPrev = x + 2 * tolerance;
-		while max(abs(x - xPrev)) > tolerance
-			xPrev = x;
-			% invJacobian = inv(jacobian(x));
-			x = x - jacobian(x) \ F(x);
-            iterations = iterations + 1;
-            xList = [xList, x]
-        end
-        disp(x);
-        disp(iterations);
-        Xrot = x;
-        iter = iterations;
-        plot(xList(1:4:iterations * 4), ...
-            xList(2:4:iterations * 4), '*', ...
-            xList(3:4:iterations * 4), ...
-            xList(4:4:iterations * 4), '+')
+    tolerance = 1e-10;
+    iterations = 0;
+
+    xPrev = [inf inf inf inf]'; % x + 2 * tolerance;
+
+    while max(abs(x - xPrev)) > tolerance
+        xPrev = x;
+        x = x - jacobian(x) \ F(x);
+        iterations = iterations + 1;
+    end
+
+    Xrot = x;
+    iter = iterations;
 end
-
-    % jacobian = @(x1, x2) [2*(x1(1)-a(1)) 2*(x1(2)-a2) 0 0;
-    %                            0 0 2*(x2(1)-b(1)) 2*(x2(2) -b2);
-    %                            2*x1(1)-a(1)-x2(1) 2*x1(2)-a2-x2(2) a(1)-x1(1) a2-x1(2);
-    %                            x2(1)-b(1) x2(2)-b2 x1(1)+b(1)-2*x2(1) x1(2)+b2-2*x2(2)];
-
-    % jacobian_fungerande = @(x1,y1,x2,y2) [2*(x1-a1) 2*(y1-a2) 0 0;
-    %                            0 0 2*(x2-b1) 2*(y2 -b2);
-    %                            2*x1-a1-x2 2*y1-a2-y2 a1-x1 a2-y1;
-    %                            x2-b1 y2-b2 x1+b1-2*x2 y1+b2-2*y2];
-    %  Indata:
-%
-%  X0 - kolumnvektor med startgissningarna (x1,y1,x2,y2)^T
-%  ra - radie för cirkel a (skalär)
-%  rb - radie för cirkel a (skalär)
-%  a  - kolumnvektor med koordinater för mittpunkt a (xa,ya)^T
-%  b  - kolumnvektor med koordinater för mittpunkt b (xb,yb)^T
-%
-%  Utdata:
-%
-%  Xrot - kolumnvektor med lösningen (x1,y1,x2,y2)^T
-%  iter - antal iterationer som använts
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% kod...
 
 %% Drawing functions
-function plot_line_from_matrix(mat)
-    line(mat(1, :), mat(2, :), 'Color', 'red')
-end
-
-function drawSphere(center, radius, fmtStr)
+function drawSphere(center, radius)
     % https://www.youtube.com/watch?v=_NAzGCYowkc&ab_channel=ANALOGTecHII
     t = 0:pi/100:2 * pi;
     x = cos(t) * radius + center(1);
     y = sin(t) * radius + center(2);
     patch(x,y, 'blue')
-% fplot(@(x) center(1) + radius * cos(x), @(y) center(2) + radius * sin(y), fmtStr)
 end
