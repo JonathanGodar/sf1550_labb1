71edddc11bd510c067b8165ccd11bb7e9ae5d9fa
Jonathan Niklasson Godar <jonathan.godar@gmail.com>
2024-02-13 08:58:15 +0100

    Major refactoring to comply with project spec

--- uppg1.m
+++ uppg1.m
@@ -2,16 +2,15 @@
 format long
 guessList = [2,2.7,4,4.8,6.2,6.7];
 f = @(x) x.^2 - 8 .* x - 12 .* sin(3.*x + 1) + 19;
-f_prim = @(x) 2 .* x - 8 - 36 * cos(3 .* x + 1); 
 
 %% 1a - plotta f(x)
-x = [-10:0.1:10];
+x = [-10:0.1:14];
 
 figure(1)
 plot(x, f(x))
 grid on
 
-% Nollstallen:
+% Nollstallen från grafen:
 % x = 2
 % x = 2.7
 % x = 4
@@ -19,97 +18,90 @@ grid on
 % x = 6.2
 % x = 6.7
 
-% kod...
-
 %% 1b - fixpunktiterationer
-for guess = guessList
-    fixpunkt(guess)
+tau = 1e-10;
+
+disp("Fixpunkt med given funtion:")
+guess = guessList(1);
+xit = fixpunkt(guess, tau);
+fprintf("Hittade rot %f med startgissning: %f och Iterationer %d\n", xit(end), guess, size(xit, 2));
+disp(abs(diff(xit)))
+
+for guess = guessList(2:end)
+    xit = fixpunkt(guess,tau);
+    fprintf("Hittade rot %f med startgissning: %f och Iterationer %d\n", xit(end), guess, size(xit, 2));
 end
 
-
-
-% kod...
+% for guess = guessList(2:end)
+%     xit = fixpunkt(guess,tau);
+%     fprintf("Hittade rot %f med startgissning: %f och Iterationer %d\n", xit(end), guess, size(xit, 2));
+% end
 
 %% 1c - Newton
-format long
-for guess = guessList(1:5)
-    output = newton(guess,f,f_prim,1e-10);
-    display([guess,output{1},output{2}])
+tau = 1e-10;
 
+disp("Newtons metod: ")
+guess = guessList(1);
+xit = newton(guess, tau);
+fprintf("Hittade rot %f med startgissning: %f och Iterationer %d\n", xit(end), guess, size(xit, 2));
+disp(abs(diff(xit)))
+
+for guess = guessList(2:end)
+    xit = newton(guess,tau);
+    fprintf("Hittade rot %f med startgissning: %f och Iterationer %d\n", xit(end), guess, size(xit, 2));
 end
-output = newton(guessList(6),f,f_prim,1e-10);
-display([guessList(6),output{1},output{2}])
-display(output{3})
 
 %% 1d - konvergensplottar
 % d1
-xStarOut = newton(2,f,f_prim,1e-15);
-xStar = xStarOut{1};
+x0 = 2;
+tau = 1e-10;
 
-newtOut = newton(2,f,f_prim,1e-15);
-fixOut = fixpunkt(2);
+x_newton15 = newton(x0,1e-15);
+x_star = x_newton15(end);
+x_fix = fixpunkt(x0, tau);
 
-xNewt = newtOut{4};
-xFix = fixOut{1};
+err_fix = abs(x_fix-x_star);
+err_newt = abs(x_newton15-x_star);
+x_vals_fix = 1:(size(err_fix,2));
+x_vals_newt = 1:(size(err_newt, 2));
 
-errFix = abs(xFix-xStar);
-errNewt = abs(xNewt-xStar);
-xSize = 1:(size(errFix,2));
 figure(2)
-semilogy(xSize,[errNewt, zeros(size(errFix, 2) - size(errNewt, 2), 1)'],xSize,errFix)
+semilogy(x_vals_newt,err_newt,x_vals_fix,err_fix)
 grid on
-
 
 %% d2
 % Plotta e_n+1 som funktion av e_n
 figure(3)
-loglog(errNewt(1:end-1), errNewt(2:end), errFix(1:end-1), errFix(2:end))
+loglog(err_newt(1:end-1), err_newt(2:end), err_fix(1:end-1), err_fix(2:end))
 grid on
 
 
-
-
-
-% end
-% b
-function rot = fixpunkt(gissning)
-	tolerance = 1e-10;
+%% b
+function xit = fixpunkt(x0, tau)
     f_fixpunkt = @(x) 1/19 * (x^2 + 11 * x - 12 * sin(3 * x + 1)) + 1;
-    
-    x = gissning;
-	x_prev = x + tolerance * 10;
-    itterations = 0;
-    xList = [];
-    while abs(x - x_prev) > tolerance
-        xList = [xList,x];
+    x = x0;
+	x_prev = x + tau * 10;
+    xit = [x0];
+    while abs(x - x_prev) > tau 
 		x_prev = x;
         x = f_fixpunkt(x);
-        itterations = itterations + 1;
+        xit = [xit, x];
     end
-    
-
-    rot{1} = xList;
-    rot{2} = itterations;
-
 end
+
 %%
 % C
-function output = newton(gissning,f,f_prim,tolerance)
-    x = gissning;
-    xPrev = x + tolerance * 2 ;
-    list = [];
-    numberOfIterations = 0;
-    xList = [];
-    while abs(x - xPrev) > tolerance
+function xit = newton(x0,tau)
+    f = @(x) x.^2 - 8 .* x - 12 .* sin(3.*x + 1) + 19;
+    f_prim = @(x) 2 .* x - 8 - 36 * cos(3 .* x + 1); 
+
+    x = x0;
+    % Skulle kunna utskiftas om x0 stor och tolerance är liten. Alternativ är att använda inf.
+    xPrev = x + tau * 10 ;
+    xit = [xPrev];
+    while abs(x - xPrev) > tau
         xPrev = x;
-        xList = [xList,x];
         x = x - f(x)/f_prim(x);
-        numberOfIterations = numberOfIterations + 1;
-        diff = abs(x-xPrev);
-        list = [list,diff];
+        xit = [xit,x];
     end
-    output{1}=x;
-    output{2}=numberOfIterations;
-    output{3}=list;
-    output{4}=xList;
 end
