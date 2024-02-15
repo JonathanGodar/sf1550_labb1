7bf9d097e3765aa95ff177da4419d7697efefce0
Alvin Daniels <alvind@kth.se>
2024-02-15 09:45:51 +0100

    Löst 3b

--- uppg3.m
+++ uppg3.m
@@ -5,11 +5,11 @@ load eiffel4.mat
 trussplot(xnod,ynod,bars);
 hold on
 index = 1;
-b = randn(size(A, 2),1);
+b = zeros(size(A, 2),1);
 b(2*index-1) = 1000;
-tic
+
 x = A\b;
-toc
+
 
 xbel = xnod + x(1:2:end); 
 ybel = ynod + x(2:2:end);
@@ -21,6 +21,39 @@ plot(xbel(index), ybel(index), 'bo', 'MarkerSize', 3)
 
 
 trussplot(xbel,ybel,bars)
+%%
+load eiffel1.mat
+e1Size = size(A,2);
+e1Time = calculationTime(A,500);
+
+load eiffel2.mat
+e2Size = size(A,2);
+e2Time = calculationTime(A,50);
+
+load eiffel3.mat
+e3Size = size(A,2);
+e3Time = calculationTime(A,25);
+
+load eiffel4.mat
+e4Size = size(A,2);
+e4Time = calculationTime(A,10);
+
+sizeList = [e1Size, e2Size, e3Size, e4Size];
+timeList = [e1Time, e2Time, e3Time, e4Time];
+%%
+loglog(sizeList,timeList)
+grid on
+hold on
+plot(sizeList(1), timeList(1), 'ro', 'MarkerSize', 3)
+plot(sizeList(2), timeList(2), 'ro', 'MarkerSize', 3)
+plot(sizeList(3), timeList(3), 'ro', 'MarkerSize', 3)
+plot(sizeList(4), timeList(4), 'ro', 'MarkerSize', 3)
+x = linspace(sizeList(1), sizeList(4));
+y = x - x(1) + timeList(1);
+% plot(x,y)
+% loglog(x,x)
+
+
 
 %% Tidtabell
 
@@ -54,4 +87,14 @@ function [jmin,jmax]=kanslighet(A,metod)
 
 % kod...
 
+end
+
+function time = calculationTime(A,iterations)
+    b = randn(size(A, 2),1);
+    tic
+    for i = [1:iterations]
+        x = A\b;
+    end
+    time = toc/iterations;
+    
 end
