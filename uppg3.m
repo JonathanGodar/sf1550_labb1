bdca24140746c9d89d55075fc3c52c105cc47ec6
Alvin Daniels <alvind@kth.se>
2024-02-15 08:56:14 +0100

    Löst 3a

--- uppg3.m
+++ uppg3.m
@@ -1,4 +1,26 @@
 % Kod för uppgift 3...
+%% a
+load eiffel4.mat
+
+trussplot(xnod,ynod,bars);
+hold on
+index = 1;
+b = randn(size(A, 2),1);
+b(2*index-1) = 1000;
+tic
+x = A\b;
+toc
+
+xbel = xnod + x(1:2:end); 
+ybel = ynod + x(2:2:end);
+disp([xbel(index),xnod(index)])
+disp([ybel(index),ybel(index)])
+
+plot(xnod(index), ynod(index), 'ro', 'MarkerSize', 3)
+plot(xbel(index), ybel(index), 'bo', 'MarkerSize', 3)
+
+
+trussplot(xbel,ybel,bars)
 
 %% Tidtabell
 
@@ -8,6 +30,7 @@
 % "Gles" och "Gles LU".
 
 % Följande kod skapar en snygg tabell med resultaten:
+
 
 tab=array2table(T,'VariableNames',{'Naiv' 'LU' 'Gles' 'Gles LU'},'RowNames',{'eiffel1' 'eiffel2' 'eiffel3' 'eiffel4'});
 disp(tab);
